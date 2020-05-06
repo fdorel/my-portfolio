8bb39158dbfff45e8677045d9bc2c13c7b1cc545
Dorel Furdui <dorelfurdui@gmail.com>
2020-05-06 18:14:45 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
             <Container fluid={true}>
                 <Row className="border-top justify-content-between p-3">
                     <Col className="p-0" md={3} sm={12}>
-                       <img src="./public/logo.png" alt="logo" />
+                       <img src="../.././public/logo.png" alt="logo" />
                     </Col>
                     <Col className="p-0 d-flex justify-content-end" md={3}>
                         Portfolio Website 2019
