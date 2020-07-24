33cab56c3eacd8f935337888a67efe519be66591
Dorel Furdui <dorelfurdui@gmail.com>
2020-07-24 12:46:34 -0400

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -8,7 +8,7 @@ function Content(props) {
  return(
     <Container fluid={true}>
         <Row className="justify-content-center">
-            <Col md={3}>
+            <Col md={12}>
                 {props.children}
             </Col>
         </Row>
