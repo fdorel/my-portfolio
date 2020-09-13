70922eb618655d24ccb21a9182ed3758751591b6
Dorel Furdui <dorelfurdui@gmail.com>
2020-09-13 17:08:38 -0400

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,6 @@ function Card(props) {
             <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc} />
             { props.item.selected && <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
         </div>
-
-div
     );
 }
 
