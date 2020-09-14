d918c527b58b35aba470f9314d588c3b6bd69c0d
Dorel Furdui <dorelfurdui@gmail.com>
2020-09-14 12:12:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Projects from './components/pages/Projects'
 import About from './components/pages/About'
 import Contact from './components/pages/Contact'
 import './App.css';
-import './App.css';
 
 class App extends React.Component {
 
