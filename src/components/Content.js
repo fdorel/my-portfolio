aecae8159b28e8e3fabdcbcab85ef1bd98243fce
Dorel Furdui <dorelfurdui@gmail.com>
2020-09-25 18:17:16 -0400

    Update Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,7 +6,7 @@ import Col from 'react-bootstrap/Col'
 function Content(props) {
 
  return(
-    <Container = {true}>
+    <Container fluid = {true}>
         <Row className="justify-content-center">
             <Col md={6}>
                 {props.children}
