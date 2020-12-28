6a4ea071e6346ef21f515f4d0e674729ff93825c
Dorel Furdui <dorelfurdui@gmail.com>
2020-12-28 14:41:43 -0500

    Update Projects.js

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../../components/Hero';
 import Carousel from '../Carousel';
 
-function Projects(props) {
+function Projects() {
     return(
         <div>
             <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
