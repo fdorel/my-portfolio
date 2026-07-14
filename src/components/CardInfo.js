9c7b90062ba934fc685521b262bf85b65f8651d6
Dorel Furdui <dorelfurdui@gmail.com>
2026-07-14 13:48:25 -0400

    Change link text from 'View' to 'Open'

--- src/components/CardInfo.js
+++ src/components/CardInfo.js
@@ -11,7 +11,7 @@ function CardInfo(props) {
         <animated.div className="g-card-info" style={style}>
             <p className="g-card-title">{props.title}</p>
             <p className="g-card-sub-title">{props.subTitle}</p>
-            <a href={props.link} target="_blank" rel="noopener noreferrer">View</a>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">Open</a>
         </animated.div>
     )
 
