c4794c5e193573ef8dfe2a9b3dc1ad4aad33dc97
Dorel Furdui <dorelfurdui@gmail.com>
2026-08-04 13:07:52 -0400

    Update link for Social Network project in Carousel
    Update link for Social Network project in Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -25,7 +25,7 @@ class Carousel extends React.Component {
                     title: 'Social Network',
                     subTitle: 'App made with React,but not finished yet...',
                     imgSrc: a1,
-                    link: 'https://sn2v.netlify.app/',
+                    link: 'https://socnet-up.netlify.app/login',
                     selected: false
                 },
                 {
